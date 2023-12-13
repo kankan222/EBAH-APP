a44a5f4f6f88d9ec9a1c09a9c1bc3634c779356f
kankan222 <jyotikankan222@gmail.com>
2023-12-13 18:42:05 +0530

    Form work done

--- static/js/main.js
+++ static/js/main.js
@@ -63,18 +63,20 @@ if(window.innerWidth < 780){
 // ACCORDION
 document.querySelectorAll(`.accordion-content`).forEach((item, index) =>{
     let header = item.querySelector(".ahead");
+    console.log(header);
     header.addEventListener("click", () => {
         item.classList.toggle("open");
         let description = item.querySelector(".adata");
+        let darr= item.querySelector(`.arrow-down`);
         if(item.classList.contains("open")){
             // description.style.height = `${description.scrollHeight}px`;
             description.classList.add(`open`);
-            document.querySelector(".arrow-down").classList.add(`open`);
+            darr.classList.add(`open`);
             
         }else{
             // description.style.height = "0px";
             description.classList.remove(`open`);
-            document.querySelector(".arrow-down").classList.remove(`open`);
+            darr.classList.remove(`open`);
         }
         removeOpen(index);
     })
@@ -84,6 +86,38 @@ function removeOpen(index1){
         if (index1 != index2) {
             item2.classList.remove ("open");
             let des = item2.querySelector(".adata");
+            des.classList.remove(`open`);
+            let arr=item2.querySelector(`.arrow-down`);
+            arr.classList.remove(`open`);
+        }
+    })
+}
+
+// EMPLOYEE ACCORDION
+document.querySelectorAll(`.assign-to`).forEach((item, index) =>{
+    let header = item.querySelector(".eaccordion");
+    header.addEventListener("click", () => {
+        item.classList.toggle("open");
+        let description = item.querySelector(".emp-acc-data");
+        let arr = item.querySelector(`.right-arr`);
+        if(item.classList.contains("open")){
+            // description.style.height = `${description.scrollHeight}px`;
+            description.classList.add(`open`);
+            arr.classList.add(`open`);
+            
+        }else{
+            // description.style.height = "0px";
+            description.classList.remove(`open`);
+            arr.classList.remove(`open`);
+        }
+        removeEmp(index);
+    })
+})
+function removeEmp(index){
+    document.querySelectorAll(`.assign-to`).forEach((item2, index2) => {
+        if (index != index2) {
+            item2.classList.remove ("open");
+            let des = item2.querySelector(".eaccordion");
             des.classList.remove(`open`);
         }
     })
@@ -112,8 +146,10 @@ function ChangeTaskStatus(ev) {
 
 //EMPLOYEE LISTS AND SUBTASKS
 function empPopup(){
-    document.querySelector(`.emp-drop-menu`).classList.toggle(`active`);
-    
+    document.querySelector(`.emp-drop-menu`).classList.toggle(`active`);   
+}
+function empRemove(){
+    document.querySelector(`.emp-drop-menu`).classList.remove(`active`);   
 }
 function subPopup(){
     document.querySelector(`.task-drop-menu`).classList.toggle(`active`);
