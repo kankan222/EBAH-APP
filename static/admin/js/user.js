90b308751fe84b42b754b28c403d3572d75fc403
kankan222 <jyotikankan222@gmail.com>
2024-01-09 04:08:53 +0530

    user-update

--- static/admin/js/user.js
+++ static/admin/js/user.js
@@ -1,8 +1,35 @@
 
-let userform= document.querySelector(`.userform`);
-    document.querySelector(`#userbtn`).addEventListener("click",()=>{
-        userform.classList.toggle(`hide`);
-    })
-    function useraction(){
-        userform.classList.add(`hide`);
+let userform = document.querySelector(`.usform`);
+let uprofile = document.querySelector(`.uprofile-settings`);
+let userProfileSettings = document.querySelector(".user-profile-settings");
+let usUpdate = document.querySelector(".update");
+let usForm = document.querySelector(`.usform`);
+let userEdit = document.querySelector(`.user-edit`);
+
+//EDIT PROFILE
+userEdit.addEventListener("click", () => {
+    uprofile.classList.remove(`hide`);
+})
+
+//UPDATE PROFILE
+usUpdate.addEventListener("click", () => {
+    uprofile.classList.add(`hide`);
+})
+
+function addUser() {
+    userform.classList.remove(`hide`);
+}
+function useraction() {
+    userform.classList.add(`hide`);
+}
+
+function handleSettingsClick(event) {
+    const clickedElement = event.target;
+    if (clickedElement.classList === "uprofile-settings" || "usform") {
+        clickedElement.classList.add(`hide`);
     }
+}
+userEdit.addEventListener("click", handleSettingsClick)
+uprofile.addEventListener("click", handleSettingsClick);
+usUpdate.addEventListener("click", handleSettingsClick);
+usForm.addEventListener("click", handleSettingsClick);
